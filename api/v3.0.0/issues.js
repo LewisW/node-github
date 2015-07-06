70929bb7dabb971424bb11e98b89934ac4a253f9
Lewis Wright <lewis@vivait.co.uk>
2015-07-06 20:47:22 +0100

    Added missing name URL parameter.

--- api/v3.0.0/issues.js
+++ api/v3.0.0/issues.js
@@ -1067,6 +1067,7 @@ var issues = module.exports = {
      *  - user (String): Required. 
      *  - repo (String): Required. 
      *  - number (Number): Required.  Validation rule: ` ^[0-9]+$ `.
+     *  - name (String): Required. 
      **/
     this.removeIssueLabel = function(msg, block, callback) {
         var self = this;
@@ -1117,6 +1118,53 @@ var issues = module.exports = {
      *  - labels (Json): Required. Array of strings - Labels to associate with this issue.
      **/
     this.replaceIssueLabels = function(msg, block, callback) {
+        var self = this;
+        this.client.httpSend(msg, block, function(err, res) {
+            if (err)
+                return self.sendError(err, null, msg, callback);
+
+            var ret;
+            try {
+                ret = res.data;
+                var contentType = res.headers["content-type"];
+                if (contentType && contentType.indexOf("application/json") !== -1)
+                    ret = JSON.parse(ret);
+            }
+            catch (ex) {
+                if (callback)
+                    callback(new error.InternalServerError(ex.message), res);
+                return;
+            }
+
+            if (!ret)
+                ret = {};
+            if (typeof ret == "object") {
+                if (!ret.meta)
+                    ret.meta = {};
+                ["x-ratelimit-limit", "x-ratelimit-remaining", "x-ratelimit-reset", "x-oauth-scopes", "link", "location", "last-modified", "etag", "status"].forEach(function(header) {
+                    if (res.headers[header])
+                        ret.meta[header] = res.headers[header];
+                });
+            }
+
+            if (callback)
+                callback(null, ret);
+        });
+    };
+
+    /** section: github
+     *  issues#removeAllIssueLabels(msg, callback) -> null
+     *      - msg (Object): Object that contains the parameters and their values to be sent to the server.
+     *      - callback (Function): function to call when the request is finished with an error as first argument and result data as second argument.
+     *
+     *  ##### Params on the `msg` object:
+     *
+     *  - headers (Object): Optional. Key/ value pair of request headers to pass along with the HTTP request. Valid headers are: 'If-Modified-Since', 'If-None-Match', 'Cookie', 'User-Agent', 'Accept', 'X-GitHub-OTP'.
+     *  - user (String): Required. 
+     *  - repo (String): Required. 
+     *  - number (Number): Required.  Validation rule: ` ^[0-9]+$ `.
+     **/
+    this.removeAllIssueLabels = function(msg, block, callback) {
         var self = this;
         this.client.httpSend(msg, block, function(err, res) {
             if (err)
